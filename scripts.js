14d4c61fef7682375b5edcfd9f18bc09d62d2463
Cesti00 <davidecestaro3030@gmail.com>
2025-07-09 15:30:12 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   const slogan = document.querySelector(".slogan");
-
-  setTimeout(() => {
-    slogan.classList.add("show");
-  }, 600); // appare dopo 0.6 secondi
+  slogan.style.opacity = "0";
 });
